8e3ea36531d145e475227143d6f9f8852975c76d
Janet Jennings <131831971+janetljennings@users.noreply.github.com>
2024-10-14 23:12:41 +0000

    Create Navbar and Alerts using Bootstrap

---
+++ app/helpers/sites_helper.rb
@@ -0,0 +1,22 @@
+module SitesHelper
+
+    def title(name,show_title = true)
+        @show_title = show_title
+        content_for(:title) do
+            name
+        end
+    end
+    
+    def nav_link(text,path)
+        if current_page?(path)
+            content_tag :li, class: "nav-item" do
+                link_to(text, path, class: "nav-link active", aria: { current: 'page'})
+            end
+        else
+            content_tag :li, class: "nav-items" do
+                link_to(text, path, class: 'nav-link')
+            end
+        end
+    end
+
+end
