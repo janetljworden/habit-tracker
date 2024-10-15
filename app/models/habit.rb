e1cb83953e2b9ade9a7ed242090ef3d82500fdf8
Janet Jennings <131831971+janetljennings@users.noreply.github.com>
2024-10-15 19:48:41 +0000

    Debug past start date validation

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,5 +2,12 @@ class Habit < ApplicationRecord
     validates :name, presence: true
     validates :frequency, presence: true
     validates :start_date, presence: true
+    validate :start_date_cannot_be_past_date
+
+    def start_date_cannot_be_past_date
+        if start_date.present? && start_date < Date.today
+            errors.add(:start_date, "Sorry! Your start date cannot be a past date")
+        end
+    end
 
 end
