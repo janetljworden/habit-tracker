9af1d2f79265e3f0a2d31a1698bcad0443c4847c
Janet Jennings <131831971+janetljennings@users.noreply.github.com>
2024-10-15 20:00:45 +0000

    Add comments to habits_controller.rb

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,13 +1,20 @@
 class HabitsController < ApplicationController
   before_action :load_task, except: [:new, :index, :create]
+  
+  # GET /habits
+  # retrieves and displays all habits
   def index
     @habits = Habit.all
   end
 
+  # GET /habits/new
+  # displays the form for creating a new habit
   def new
     @habit = Habit.new
   end
 
+  # POST /habits
+  # submits the form data to create a new habit
   def create
     @habit = Habit.new(habit_params)
     if @habit.save
@@ -17,12 +24,18 @@ class HabitsController < ApplicationController
     end
   end
 
+  # GET /habits/:id
+  # displays a specific habit by its ID
   def show
   end
 
+  # GET /habits/:id/edit
+  # displays the form for editing a specific habit
   def edit
   end
 
+  # PATCH /habits/:id
+  # updates the specific habit with the submitted form data
   def update
     if @habit.update habit_params
       redirect_to @habit, notice: "Habit Updated."
@@ -31,6 +44,8 @@ class HabitsController < ApplicationController
     end
   end
 
+  # DELETE /habits/:id
+  # deletes a specific habit by its ID
   def destroy
     @habit.destroy
     redirect_to habits_path, alert: "Habit Deleted."
@@ -38,10 +53,12 @@ class HabitsController < ApplicationController
 
   private
 
+  # allow specific attributes for mass assignment
   def habit_params
     params.require(:habit).permit(:name,:frequency,:start_date)
   end
 
+  # loads a specific habit based on its ID
   def load_task
     @habit = Habit.find params[:id]
   end
