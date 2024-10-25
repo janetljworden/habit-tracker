f98b5c0c3bf770c4273959b80c3b8fb7661115e9
Janet Jennings <131831971+janetljennings@users.noreply.github.com>
2024-10-25 05:34:06 +0000

    Reformat pages

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -7,7 +7,7 @@ class Habit < ApplicationRecord
     validate :start_date_cannot_be_past_date
 
     def start_date_cannot_be_past_date
-        if start_date.present? && start_date < Date.today
+        if start_date < Date.today
             errors.add(:start_date, "Sorry! Your start date cannot be a past date")
         end
     end
